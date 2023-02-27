2104e96ec3cea7d70bbe38c6d964b833e2c0a2a9
rhams1997 <88038755+rhams1997@users.noreply.github.com>
2023-02-27 15:30:13 -0300

    first commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+export default {
+    colors: {
+        primary: '#0B420C',
+        primary_light: '#12A454',
+        secondary: 'C8191E',
+        background: '#E5E5E5',
+        headerText:'#FFFFFF',
+        text: '#000000',
+        text_light: '#969CB2',
+        highlight: '#FF872C',
+        card_background: '#363F5F'
+    },
+    fonts: {
+
+    }
+}
