e942f509528233b6aa7768bad50acbf0e936e3f3
rhams1997 <88038755+rhams1997@users.noreply.github.com>
2023-03-07 16:33:57 -0300

    commit

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,11 +1,11 @@
-
 import styled from 'styled-components/native';
 
 
-export const Container = styled.View'
+
+export const Container = styled.View`
   flex: 1;
   background-color: '#fff';
-  aling-items: 'center';
+  align-items: 'center';
   justify-content: 'center';
-';
+  `;
       
