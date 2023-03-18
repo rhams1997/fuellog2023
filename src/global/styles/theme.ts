bf26ebcc3eead490cd702914251a2cae093e8dfa
rhams1997 <88038755+rhams1997@users.noreply.github.com>
2023-03-17 21:03:04 -0300

    commit

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -4,7 +4,7 @@ export default {
         primary_light: '#12A454',
         secondary: 'C8191E',
         background: '#E5E5E5',
-        header_text:'#FFFFFF',
+        header_text:'#000000',
         text: '#000000',
         text_light: '#969CB2',
         highlight: '#FF872C',
